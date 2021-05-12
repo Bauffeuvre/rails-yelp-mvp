861ebf4412d3c10ad6787fd4f37bc0a13755be92
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-05-12 12:26:45 -0400

    Finished, Added nested routes for reviews, update show page restaurants to include reviews, create form + method to create reviews

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,36 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+  
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:success] = "Object successfully created"
+      redirect_to restaurant_path(@restaurant)
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+    
+  def show
+  @reviews = Review.where(params[:id])
+  end
+  
+  private
+  
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,29 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+  
+  def create
+    # Create the review
+    @review = Review.new(reviews_params)
+    # Get the restaurant Id
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    # Set the review TO THE restaurant
+    @review.restaurant = @restaurant
+    if @review.save
+      flash[:success] = "Object successfully created"
+      redirect_to restaurant_path(@restaurant)
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  private
+
+  def reviews_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
