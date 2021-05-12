3f6e4ca615044a30e93466902e44885cf2635a78
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-05-12 11:01:56 -0400

    Added the adress to the restaurants table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_145725) do
+ActiveRecord::Schema.define(version: 2021_05_12_150127) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_05_12_145725) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
   create_table "reviews", force: :cascade do |t|
