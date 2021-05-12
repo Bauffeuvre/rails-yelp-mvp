495790d09a7c76dde21e5aaae8dfe3c4b10310db
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-05-12 11:25:27 -0400

    Added seed (5 restaurants)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating 5 Restaurants..."
+
+5.times do
+  name = Faker::Restaurant.name
+  address = Faker::Address.street_address
+  category = ["chinese", "italian", "japanese", "french", "belgian"].sample
+  restaurant = Restaurant.create!(name:name ,address: address, category: category )
+  puts "Creating Restaurant #{restaurant.id}"
+end
+
+puts "Finished !"
