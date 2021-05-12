ad44ceda624a7286f854488e277799e3399c79d3
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-05-12 11:17:05 -0400

    Added Restaurants Controller + Reviews Controller + Validations for the models

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_150127) do
+ActiveRecord::Schema.define(version: 2021_05_12_150603) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
